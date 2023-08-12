bef3ab8c10af6cb945ebf3fc0d66d732855a6f28
Lucas Mendes <123208323+dataGopher@users.noreply.github.com>
2023-08-12 11:03:56 -0300

    feature
    datonomy kit framework for ios

---
+++ DatonomyKit.xcframework/ios-arm64/DatonomyKit.framework/Headers/DatonomyKit.h
@@ -0,0 +1,19 @@
+//
+//  DatonomyKit.h
+//  DatonomyKit
+//
+//  Created by kelveng carlisson on 24/02/23.
+//
+
+#import <Foundation/Foundation.h>
+#import <DatonomyKit/DatonomyKit.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
